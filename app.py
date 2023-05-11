4ba3b92e4ec5d190ea83b247538ae4a3bb778110
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2023-05-11 18:02:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 import streamlit as st
-#from Zeeshann1/YOLOv5-streamlit-Webapp/edit/main import detection_server
-import requests
-import json
+import torch
+from PIL import Image
+from torchvision.transforms import ToTensor
+from yolov5.utils.plots import plot_one_box
+from yolov5.models.experimental import attempt_load
+from yolov5.utils.datasets import letterbox
 
-# Streamlit app header and description
-st.title("YOLOv5 Object Detection")
-st.write("Upload an image and perform object detection using YOLOv5.")
+st.set_page_config(page_title="YOLOv5 Object Detection", page_icon=":guardsman:", layout="wide")
 
-# File uploader
-uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = attempt_load("yolov5s.pt", map_location=torch.device('cpu'))
+    return model
 
-if uploaded_image is not None:
-    # Send image to detection endpoint
-    files = {'file': uploaded_image.getvalue()}
-    response = requests.post('http://localhost:8501/detect', files=files)
-    st.write(response.status_code)
-    # Check if the request was successful
-    if response.status_code == 405:
-        # Parse the response
-        results = json.loads(response.text)
+def detect(image):
+    model = load_model()
+    img = Image.open(image)
+    img = img.convert("RGB")
+    img = letterbox(img, new_shape=640)[0]
+    img = ToTensor()(img)
+    img = img.unsqueeze(0)
+    pred = model(img)[0]
+    for det in pred:
+        plot_one_box(det[:4], det[4], color=(0, 255, 0), line_thickness=2)
 
-        # Display the detected objects
-        for obj in results['objects']:
-            st.write("Class:", obj['class'])
-            st.write("Confidence:", obj['confidence'])
-            st.image(obj['image'], use_column_width=True)
-    else:
-        st.error("Error occurred during object detection.")
+def main():
+    st.title("YOLOv5 Object Detection")
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        if st.button("Detect Objects"):
+            detect(uploaded_file)
+            st.image(image, caption="Annotated Image", use_column_width=True)
+
+if __name__ == "__main__":
+    main()

d03023aa0cd0adfe8bc8a04ba960878685306b31
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2023-05-11 11:37:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from YOLOv5-streamlit-Webapp import detection_server
+from Zeeshann1/YOLOv5-streamlit-Webapp/edit/main import detection_server
 import requests
 import json
 

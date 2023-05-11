23233b5df7e5763b478ebcce645e210e92367eab
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2023-05-11 10:57:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,27 @@
 import streamlit as st
-import torch
-import cv2
+import requests
+import json
 
+# Streamlit app header and description
 st.title("YOLOv5 Object Detection")
 st.write("Upload an image and perform object detection using YOLOv5.")
 
-
-# Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-  
-# set model parameters
-model.conf = 0.35  # NMS confidence threshold
-model.iou = 0.45  # NMS IoU threshold
-model.agnostic = False  # NMS class-agnostic
-model.multi_label = False  # NMS multiple labels per box
-model.max_det = 1000  # maximum number of detections per image
-
-
-
-
-def run_inference(image):
-    # perform inference
-    results = model(image)
-
-    # parse results
-    predictions = results.pred[0]
-    boxes = predictions[:, :4] # x1, y1, x2, y2
-    scores = predictions[:, 4]
-    categories = predictions[:, 5]
-
-    # show detection bounding boxes on image
-    results.show()
-
-    # save results into "results/" folder
-    results.save(save_dir='results/')
-
-
-
-
+# File uploader
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
-    # Read the image file
-    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    # Send image to detection endpoint
+    files = {'file': uploaded_image.getvalue()}
+    response = requests.post('http://localhost:5000/detect', files=files)
 
-    # Perform inference
-    run_inference(image)
+    if response.status_code == 200:
+        # Parse the response
+        results = json.loads(response.text)
 
-
-
-if __name__ == '__main__':
-    app.run()
+        # Display the detected objects
+        for obj in results['objects']:
+            st.write("Class:", obj['class'])
+            st.write("Confidence:", obj['confidence'])
+            st.image(obj['image'], use_column_width=True)
+    else:
+        st.error("Error occurred during object detection.")
